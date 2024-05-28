e56d0632b271994b93bdec632e00ee6830e362ff
JWKim119 <jaewonk7@gmail.com>
2024-05-28 10:23:27 +0900

    스프링공부

---
+++ springboot-developer/src/main/java/me/jaewon/springbootdeveloper/test01.java
@@ -0,0 +1,9 @@
+package me.jaewon.springbootdeveloper;
+
+import org.springframework.stereotype.Component;
+
+
+public abstract class test01 {
+private test02 test02;
+}
+
---
+++ springboot-developer/src/main/java/me/jaewon/springbootdeveloper/test02.java
@@ -0,0 +1,7 @@
+package me.jaewon.springbootdeveloper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class test02 {
+
+}
